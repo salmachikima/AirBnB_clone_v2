a504116210048f69fa568dc29864006e40b2cdfc
salma <salmamarokti888@gmail.com>
2025-01-26 17:42:28 -0800

    bfhrbvw

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that distributes an archive to your web servers based on task 1"""
+
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """ destribute archive to the webservers"""
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    r = local("sudo tar -cvzf {} web_static".format(filename))
+    if r.succeeded:
+        return filename
+    else:
+        return None
