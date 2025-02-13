5393105a473495549f8e57bd5cf826c60df096ac
salmachikima <salmamarokti888@gmail.com>
2025-02-12 16:07:38 -0800

    vgrfbrhjf

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""Flask web application
 """
-from models import storage
+Flask web application
+"""
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displaying an HTML page with list of states/related cities
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove SQLAlchemy"""
+def teardown_db(_):
+    """closing the storage on teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displaying the cities by its states"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
