3adb42e43013655e7f18c2c3f943c687d8bbd24e
salmachikima <salmamarokti888@gmail.com>
2025-02-12 16:16:09 -0800

    sukuna

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,27 +2,28 @@
 """
 Flask web application
 """
-
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """displaying states /cities arranged in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closeing the storage"""
+def teardown_db(_):
+    """Closing the storage"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states(id=None):
+    """cities and states in alpha order"""
+    states = storage.all(State, id)
+    return render_template("9-states.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
